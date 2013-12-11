1521a11613b1063978166745d7b4cef628ea282a
Ferchulin6489 <Ferchulin_64_89@hotmail.com>
2013-12-11 10:51:43 -0200

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -44,7 +44,7 @@ public class AsynchIOServer
         tcpListener.Start();
         Console.WriteLine("************This is Server program************");
         Console.WriteLine("Hoe many clients are going to connect to this server?:");
-        int numberOfClientsYouNeedToConnect =int.Parse( Console.ReadLine());
+        int numberOfClientsYouNeedToConnect = 5; //int.Parse( Console.ReadLine());
         for (int i = 0; i < numberOfClientsYouNeedToConnect; i++)
         {
             Thread newThread = new Thread(new ThreadStart(Listeners));
